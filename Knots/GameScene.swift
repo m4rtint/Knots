e177b65e7b363450c3da3e668799728d6526abf9
Martin <m4rtin.t@gmail.com>
2017-08-21 15:37:13 -0700

    Set up rocks onto the screen programmatically so that it sticks to the corner of the screen

--- Knots/GameScene.swift
+++ Knots/GameScene.swift
@@ -7,104 +7,51 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+    let rock = SKTexture(imageNamed: "rock")
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    override func didMove(to view: SKView) {
+        setupSceneObjects()
+    }
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    //Set up Rocks on the corner of the screens
+    func setupSceneObjects() {
+        //Set the x+y coordinate
+        //Top Left
+        var xCoordinate:CGFloat = -(self.size.width/2)+(rock.size().width/2)
+        var yCoordinate:CGFloat = (self.size.height/2)-(rock.size().height/2)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        var node = SKSpriteNode(texture: rock)
+        node.position = CGPoint(x: xCoordinate, y:yCoordinate)
+        addChild(node)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        //Top Right
+        xCoordinate = (self.size.width/2)-(rock.size().width/2)
+        yCoordinate = (self.size.height/2)-(rock.size().height/2)
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
+        node = SKSpriteNode(texture: rock)
+        node.position = CGPoint(x: xCoordinate, y:yCoordinate)
+        addChild(node)
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
+        //Bottom left
+        xCoordinate = -(self.size.width/2)+(rock.size().width/2)
+        yCoordinate = -(self.size.height/2)+(rock.size().height/2)
         
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
+        node = SKSpriteNode(texture: rock)
+        node.position = CGPoint(x: xCoordinate, y:yCoordinate)
+        addChild(node)
         
-        self.lastUpdateTime = currentTime
+        //Bottom Right
+        xCoordinate = (self.size.width/2)-(rock.size().width/2)
+        yCoordinate = -(self.size.height/2)+(rock.size().height/2)
+        
+        node = SKSpriteNode(texture: rock)
+        node.position = CGPoint(x: xCoordinate, y:yCoordinate)
+        addChild(node)
     }
+
 }
+    
+
+
