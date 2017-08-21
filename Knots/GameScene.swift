47cd65ae42f607bfdaab951f493b619a4ee835ff
Martin <m4rtin.t@gmail.com>
2017-08-21 16:44:46 -0700

    Cone of Light Rotation from user input

--- Knots/GameScene.swift
+++ Knots/GameScene.swift
@@ -9,10 +9,22 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    //Static Variables
     let rock = SKTexture(imageNamed: "rock")
+    let DegreesToRadians = CGFloat.pi / 180
+    var light = SKSpriteNode()
+    
+    //Tunable Variables
+    let lightHouseRotationTimeTaken:Double = 1
+    
     
     override func didMove(to view: SKView) {
+        //Setup Corner rocks
         setupSceneObjects()
+        
+        //Set up cone of light on scene
+        self.light = self.childNode(withName: "ConeOfLight") as! SKSpriteNode
+        
     }
     
     //Set up Rocks on the corner of the screens
@@ -51,6 +63,33 @@ class GameScene: SKScene {
         node.position = CGPoint(x: xCoordinate, y:yCoordinate)
         addChild(node)
     }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let curTouch = touches.first!
+        let curPoint = curTouch.location(in: self)
+        
+        rotateLight(currentPoint: curPoint)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let curTouch = touches.first!
+        let curPoint = curTouch.location(in: self)
+        
+        rotateLight(currentPoint: curPoint)
+    }
+    
+    func rotateLight (currentPoint:CGPoint ) {
+        let deltaX = self.light.position.x - currentPoint.x
+        let deltaY = self.light.position.y - currentPoint.y
+        
+        let angle = atan2(deltaY,deltaX) + 270 * DegreesToRadians
+        
+        let rotate = SKAction.rotate(toAngle: angle, duration:lightHouseRotationTimeTaken, shortestUnitArc: true)
+        self.light.run(rotate)
+    }
+
 
 }
     
